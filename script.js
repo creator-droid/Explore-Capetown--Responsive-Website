e688725acf7b51f3c44d7e729c664313a8e81745
Siphosihle Nomfanelo Mtshali <106113368+creator-droid@users.noreply.github.com>
2024-01-09 20:18:48 +0200

    updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
+    console.log("Scroll event detected!");
     var navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scroll-navbar", window.scrollY > 0);
   });
